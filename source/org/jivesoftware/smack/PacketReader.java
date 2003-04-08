68d489ef79fed9fab459c2f7f3f44a1856973944
Matt Tucker <matt@jivesoftware.com>
2003-04-08 21:22:09 +0000

    Fix from John for bad packet processing logic.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@1869 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smack/PacketReader.java
+++ source/org/jivesoftware/smack/PacketReader.java
@@ -311,7 +311,7 @@ class PacketReader {
 
         // Remove all null values from the collectors list.
         synchronized (collectors) {
-            for (int i=collectors.size()-1; i<=0; i--) {
+            for (int i=collectors.size()-1; i>=0; i--) {
                     if (collectors.get(i) == null) {
                         collectors.remove(i);
                     }
