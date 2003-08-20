a32776bd89d127ebfaeef911bb6da298d76ddce9
Matt Tucker <matt@jivesoftware.com>
2003-08-20 14:56:03 +0000

    Fixed provider methods.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@2046 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/PrivateDataManager.java
+++ source/org/jivesoftware/smackx/PrivateDataManager.java
@@ -95,11 +95,11 @@ public class PrivateDataManager {
      *
      * @param elementName the XML element name.
      * @param namespace the XML namespace.
-     * @return the IQ provider.
+     * @return the PrivateData provider.
      */
-    public static Object getPrivateDataProvider(String elementName, String namespace) {
+    public static PrivateData getPrivateDataProvider(String elementName, String namespace) {
         String key = getProviderKey(elementName, namespace);
-        return privateDataProviders.get(key);
+        return (PrivateData)privateDataProviders.get(key);
     }
 
     /**
@@ -108,10 +108,10 @@ public class PrivateDataManager {
      *
      * @param elementName the XML element name.
      * @param namespace the XML namespace.
-     * @param provider the IQ provider.
+     * @param provider the private data provider.
      */
     public static void addPrivateDataProvider(String elementName, String namespace,
-            IQProvider provider)
+            PrivateDataProvider provider)
     {
         String key = getProviderKey(elementName, namespace);
         privateDataProviders.put(key, provider);
