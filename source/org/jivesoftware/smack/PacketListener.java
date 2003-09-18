690ba2f345724e5d58c66bb0ef1c4222c5a3d487
Matt Tucker <matt@jivesoftware.com>
2003-09-18 20:38:12 +0000

    Added Javadoc note about single thread used for all listeners.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@2088 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smack/PacketListener.java
+++ source/org/jivesoftware/smack/PacketListener.java
@@ -68,7 +68,11 @@ import org.jivesoftware.smack.filter.PacketFilter;
 public interface PacketListener {
 
     /**
-     * Process the next packet sent to this packet listener.
+     * Process the next packet sent to this packet listener.<p>
+     *
+     * A single thread is responsible for invoking all listeners, so
+     * it's very important that implementations of this method not block
+     * for any extended period of time.
      *
      * @param packet the packet to process.
      */
