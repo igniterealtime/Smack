14034110bf5ad100d30a9cd2b18b21bfdd8147ba
Matt Tucker <matt@jivesoftware.com>
2004-03-15 21:56:18 +0000

    Small fix.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@2254 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/debugger/EnhancedDebuggerWindow.java
+++ source/org/jivesoftware/smackx/debugger/EnhancedDebuggerWindow.java
@@ -227,7 +227,7 @@ class EnhancedDebuggerWindow {
         versionPanel.setLayout(new BoxLayout(versionPanel, BoxLayout.X_AXIS));
         versionPanel.setMaximumSize(new Dimension(2000, 31));
         versionPanel.add(new JLabel(" Smack version: "));
-        JFormattedTextField field = new JFormattedTextField(SmackConfiguration.getVersionNumber());
+        JFormattedTextField field = new JFormattedTextField(SmackConfiguration.getVersion());
         field.setEditable(false);
         field.setBorder(null);
         versionPanel.add(field);
