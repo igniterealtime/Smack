c34861903df51d77b874cfd948c1a853fa78d984
Gaston Dombiak <gaston@jivesoftware.com>
2004-04-05 16:24:44 +0000

    1. Adds class and methods comments 2. Fixes parsing errors (#getText was replaced with #nextText)
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@2276 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/provider/MUCOwnerProvider.java
+++ source/org/jivesoftware/smackx/provider/MUCOwnerProvider.java
@@ -59,7 +59,7 @@ import org.jivesoftware.smackx.packet.MUCOwner;
 import org.xmlpull.v1.XmlPullParser;
 
 /**
- * Represents .....
+ * The MUCOwnerProvider parses MUCOwner packets. (@see MUCOwner)
  * 
  * @author Gaston Dombiak
  */
@@ -109,7 +109,7 @@ public class MUCOwnerProvider implements IQProvider {
                     item.setActor(parser.getAttributeValue("", "jid"));
                 }
                 if (parser.getName().equals("reason")) {
-                    item.setReason(parser.getText());
+                    item.setReason(parser.nextText());
                 }
             }
             else if (eventType == XmlPullParser.END_TAG) {
@@ -129,7 +129,7 @@ public class MUCOwnerProvider implements IQProvider {
             int eventType = parser.next();
             if (eventType == XmlPullParser.START_TAG) {
                 if (parser.getName().equals("reason")) {
-                    destroy.setReason(parser.getText());
+                    destroy.setReason(parser.nextText());
                 }
             }
             else if (eventType == XmlPullParser.END_TAG) {
