c395074c86bd4aeb8714a7bc6e666669991de824
Gaston Dombiak <gaston@jivesoftware.com>
2004-09-12 02:11:52 +0000

    Fixes incorrect namespace for granting and revoking admin/owner privileges.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@2379 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/muc/MultiUserChat.java
+++ source/org/jivesoftware/smackx/muc/MultiUserChat.java
@@ -1222,11 +1222,11 @@ public class MultiUserChat {
      * @throws XMPPException if an error occurs granting ownership privileges to a user.
      */
     public void grantOwnership(String jid) throws XMPPException {
-        MUCAdmin iq = new MUCAdmin();
+        MUCOwner iq = new MUCOwner();
         iq.setTo(room);
         iq.setType(IQ.Type.SET);
         // Set the affiliation to "owner". This will grant a user's ownership
-        MUCAdmin.Item item = new MUCAdmin.Item("owner", null);
+        MUCOwner.Item item = new MUCOwner.Item("owner");
         item.setJid(jid);
         iq.addItem(item);
 
@@ -1257,11 +1257,11 @@ public class MultiUserChat {
      * @throws XMPPException if an error occurs granting ownership privileges to a user.
      */
     public void revokeOwnership(String jid) throws XMPPException {
-        MUCAdmin iq = new MUCAdmin();
+        MUCOwner iq = new MUCOwner();
         iq.setTo(room);
         iq.setType(IQ.Type.SET);
         // Set the affiliation to "admin". This will revoke a user's ownership
-        MUCAdmin.Item item = new MUCAdmin.Item("admin", null);
+        MUCOwner.Item item = new MUCOwner.Item("admin");
         item.setJid(jid);
         iq.addItem(item);
 
@@ -1293,11 +1293,11 @@ public class MultiUserChat {
      * @throws XMPPException if an error occurs granting administrator privileges to a user.
      */
     public void grantAdmin(String jid) throws XMPPException {
-        MUCAdmin iq = new MUCAdmin();
+        MUCOwner iq = new MUCOwner();
         iq.setTo(room);
         iq.setType(IQ.Type.SET);
         // Set the affiliation to "admin". This will grant administrator privileges
-        MUCAdmin.Item item = new MUCAdmin.Item("admin", null);
+        MUCOwner.Item item = new MUCOwner.Item("admin");
         item.setJid(jid);
         iq.addItem(item);
 
@@ -1329,11 +1329,11 @@ public class MultiUserChat {
      * @throws XMPPException if an error occurs revoking administrator privileges from a user.
      */
     public void revokeAdmin(String jid) throws XMPPException {
-        MUCAdmin iq = new MUCAdmin();
+        MUCOwner iq = new MUCOwner();
         iq.setTo(room);
         iq.setType(IQ.Type.SET);
         // Set the affiliation to "member". This will revoke a user's ownership
-        MUCAdmin.Item item = new MUCAdmin.Item("member", null);
+        MUCOwner.Item item = new MUCOwner.Item("member");
         item.setJid(jid);
         iq.addItem(item);
 
