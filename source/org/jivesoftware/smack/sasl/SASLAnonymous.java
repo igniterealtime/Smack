5a7c902ef3ab94ae2562fa4f6440273a194c704b
Gaston Dombiak <gaston@jivesoftware.com>
2005-09-05 21:37:27 +0000

    Initial version. SMACK-84
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@2780 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ source/org/jivesoftware/smack/sasl/SASLAnonymous.java
@@ -0,0 +1,51 @@
+/**
+ * $RCSfile$
+ * $Revision: $
+ * $Date: $
+ *
+ * Copyright 2003-2005 Jive Software.
+ *
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jivesoftware.smack.sasl;
+
+import org.jivesoftware.smack.SASLAuthentication;
+
+/**
+ * Implementation of the SASL ANONYMOUS mechanisn as defined by the
+ * <a href="http://www.ietf.org/internet-drafts/draft-ietf-sasl-anon-05.txt">IETF draft
+ * document</a>.
+ *
+ * @author Gaston Dombiak
+ */
+public class SASLAnonymous extends SASLMechanism {
+
+    public SASLAnonymous(SASLAuthentication saslAuthentication) {
+        super(saslAuthentication);
+    }
+
+    protected String getName() {
+        return "ANONYMOUS";
+    }
+
+    protected String getAuthenticationText(String username, String host, String password) {
+        // Nothing to send in the <auth> body
+        return null;
+    }
+
+    protected String getChallengeResponse(byte[] bytes) {
+        // Return null since this mechanism will never get a challenge from the server
+        return null;
+    }
+}
