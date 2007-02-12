98bf0d8cfb121b28ab4e8cb2744f625845c755b0
Matt Tucker <matt@jivesoftware.com>
2007-02-12 00:59:05 +0000

    Copyright update.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@7071 b35dd754-fafc-0310-a699-88a17e54d16e

--- jingle/extension/source/org/jivesoftware/smackx/packet/Jingle.java
+++ jingle/extension/source/org/jivesoftware/smackx/packet/Jingle.java
@@ -3,7 +3,7 @@
  * $Revision: 2407 $
  * $Date: 2004-11-02 23:37:00 +0000 (Tue, 02 Nov 2004) $
  *
- * Copyright 2003-2004 Jive Software.
+ * Copyright 2003-2007 Jive Software.
  *
  * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- source/org/jivesoftware/smack/ConnectionConfiguration.java
+++ source/org/jivesoftware/smack/ConnectionConfiguration.java
@@ -3,7 +3,7 @@
  * $Revision: 3306 $
  * $Date: 2006-01-16 14:34:56 -0300 (Mon, 16 Jan 2006) $
  *
- * Copyright 2003-2004 Jive Software.
+ * Copyright 2003-2007 Jive Software.
  *
  * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
