8775331293ba49ec9d84a89b065c5904706b1b9e
Thiago Camargo <thiago@b35dd754-fafc-0310-a699-88a17e54d16e>
2007-02-28 22:08:39 +0000

    Increase TimeOut Checking
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@7327 b35dd754-fafc-0310-a699-88a17e54d16e

--- jingle/extension/source/org/jivesoftware/smackx/jingle/nat/TransportResolver.java
+++ jingle/extension/source/org/jivesoftware/smackx/jingle/nat/TransportResolver.java
@@ -89,7 +89,7 @@ public abstract class TransportResolver {
     public Type type = Type.rawupd;
 
     // the time, in milliseconds, before a check aborts
-    public static final int CHECK_TIMEOUT = 2000;
+    public static final int CHECK_TIMEOUT = 4000;
 
     // Listeners for events
     private final ArrayList<TransportResolverListener> listeners = new ArrayList<TransportResolverListener>();
