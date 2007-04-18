5cd266befbf593811049750d6402e9214c469ec8
Thiago Camargo <thiago@b35dd754-fafc-0310-a699-88a17e54d16e>
2007-04-18 17:30:14 +0000

    Remove unused imports.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@8058 b35dd754-fafc-0310-a699-88a17e54d16e

--- jingle/extension/test/org/jivesoftware/smackx/jingle/nat/LocalhostTest.java
+++ jingle/extension/test/org/jivesoftware/smackx/jingle/nat/LocalhostTest.java
@@ -2,12 +2,6 @@ package org.jivesoftware.smackx.jingle.nat;
 
 import junit.framework.TestCase;
 
-import java.util.Enumeration;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.InetAddress;
-import java.net.DatagramSocket;
-
 public class LocalhostTest extends TestCase {
 
     public void testGetLocalhost() {
