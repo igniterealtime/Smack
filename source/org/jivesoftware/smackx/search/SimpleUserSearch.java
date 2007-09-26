85a92b600b28c8425e2c191227975fb7118a214e
Matt Tucker <matt@jivesoftware.com>
2007-09-26 23:14:51 +0000

    License header fixes.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@9227 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/search/SimpleUserSearch.java
+++ source/org/jivesoftware/smackx/search/SimpleUserSearch.java
@@ -1,13 +1,19 @@
 /**
- * $RCSfile: ,v $
- * $Revision: $
- * $Date:  $
+ * Copyright 2003-2007 Jive Software.
  *
- * Copyright (C) 1999-2005 Jive Software. All rights reserved.
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This software is the proprietary information of Jive Software.
- * Use is subject to license terms.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.jivesoftware.smackx.search;
 
 import org.jivesoftware.smack.packet.IQ;
--- source/org/jivesoftware/smackx/workgroup/settings/SearchSettings.java
+++ source/org/jivesoftware/smackx/workgroup/settings/SearchSettings.java
@@ -1,14 +1,18 @@
 /**
- * $RCSfile$
- * $Revision: 38648 $
- * $Date: 2006-12-27 01:46:18 -0800 (Wed, 27 Dec 2006) $
+ * Copyright 2003-2007 Jive Software.
  *
- * Copyright (C) 1999-2005 Jive Software. All rights reserved.
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This software is the proprietary information of Jive Software. Use is
- subject to license terms.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.jivesoftware.smackx.workgroup.settings;
 
 import org.jivesoftware.smackx.workgroup.util.ModelUtil;
