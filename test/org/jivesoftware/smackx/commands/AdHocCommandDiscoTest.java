ffb8cd914ab341e8679a5e4078b4fe466fcbe3af
Matt Tucker <matt@jivesoftware.com>
2008-11-14 00:59:48 +0000

    Fixed test.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@10882 b35dd754-fafc-0310-a699-88a17e54d16e

--- test/org/jivesoftware/smackx/commands/AdHocCommandDiscoTest.java
+++ test/org/jivesoftware/smackx/commands/AdHocCommandDiscoTest.java
@@ -76,10 +76,9 @@ public class AdHocCommandDiscoTest extends SmackTestCase {
             manager1.registerCommand("test", "test node", LocalCommand.class);
             
             AdHocCommandManager manager2 = AdHocCommandManager.getAddHocCommandsManager(getConnection(1));
-            DiscoverItems items = manager2.discoverCommands(getBareJID(0));
+            DiscoverItems items = manager2.discoverCommands(getFullJID(0));
 
-            System.out.println(items.toXML());
-            //TODO: test that "test" node is in disco items
+            assertTrue("Disco for command test failed", items.getItems().next().getNode().equals("test"));
         }
         catch (Exception e) {
             fail(e.getMessage());
