50bf29b1223f8b387e5d63e5371f22e9d69e27ec
rcollier <rcollier@b35dd754-fafc-0310-a699-88a17e54d16e>
2011-07-04 19:51:43 +0000

    Updates for Release of 3.2.1
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/branches/smack_3_2_0@12557 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smack/SmackConfiguration.java
+++ source/org/jivesoftware/smack/SmackConfiguration.java
@@ -44,7 +44,7 @@ import java.util.*;
  */
 public final class SmackConfiguration {
 
-    private static final String SMACK_VERSION = "3.2.0";
+    private static final String SMACK_VERSION = "3.2.1";
 
     private static int packetReplyTimeout = 5000;
     private static int keepAliveInterval = 30000;
