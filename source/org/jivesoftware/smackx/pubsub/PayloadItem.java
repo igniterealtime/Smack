98d03d2d7558126b5ffb19f5e3e3fa12538ce6e2
rcollier <rcollier@b35dd754-fafc-0310-a699-88a17e54d16e>
2011-08-15 01:28:15 +0000

    Just cleaning up an import used in the javadoc.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/branches/smack_3_2_0@12575 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/pubsub/PayloadItem.java
+++ source/org/jivesoftware/smackx/pubsub/PayloadItem.java
@@ -13,10 +13,9 @@
  */
 package org.jivesoftware.smackx.pubsub;
 
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.PacketExtension;
 import org.jivesoftware.smackx.pubsub.provider.ItemProvider;
-
-import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
 
 /**
  * This class represents an item that has been, or will be published to a
