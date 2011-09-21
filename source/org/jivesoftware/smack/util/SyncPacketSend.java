c96be693dc06a42296c806a1c16c35588ca56049
rcollier <rcollier@b35dd754-fafc-0310-a699-88a17e54d16e>
2011-09-21 23:57:34 +0000

    Moved utility class to the core, since it is not extension related and useful for all code.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/branches/smack_3_2_0@12598 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smack/util/SyncPacketSend.java
+++ source/org/jivesoftware/smack/util/SyncPacketSend.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jivesoftware.smackx.packet;
+package org.jivesoftware.smack.util;
 
 import org.jivesoftware.smack.PacketCollector;
 import org.jivesoftware.smack.SmackConfiguration;
--- source/org/jivesoftware/smackx/bytestreams/ibb/InBandBytestreamSession.java
+++ source/org/jivesoftware/smackx/bytestreams/ibb/InBandBytestreamSession.java
@@ -33,12 +33,12 @@ import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smack.packet.PacketExtension;
 import org.jivesoftware.smack.packet.XMPPError;
 import org.jivesoftware.smack.util.StringUtils;
+import org.jivesoftware.smack.util.SyncPacketSend;
 import org.jivesoftware.smackx.bytestreams.BytestreamSession;
 import org.jivesoftware.smackx.bytestreams.ibb.packet.Close;
 import org.jivesoftware.smackx.bytestreams.ibb.packet.Data;
 import org.jivesoftware.smackx.bytestreams.ibb.packet.DataPacketExtension;
 import org.jivesoftware.smackx.bytestreams.ibb.packet.Open;
-import org.jivesoftware.smackx.packet.SyncPacketSend;
 
 /**
  * InBandBytestreamSession class represents an In-Band Bytestream session.
