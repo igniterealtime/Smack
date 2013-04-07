4155e9ca809d9be35aabe632b41be0e82a2e8ee3
rcollier <rcollier@b35dd754-fafc-0310-a699-88a17e54d16e>
2013-04-07 13:17:26 +0000

    SMACK-391 Reformat some new files to Smack standard
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/branches/smack_3_3_0@13602 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smack/util/DateFormatType.java
+++ source/org/jivesoftware/smack/util/DateFormatType.java
@@ -23,42 +23,43 @@ import java.text.SimpleDateFormat;
 
 /**
  * Defines the various date and time profiles used in XMPP along with their associated formats.
+ * 
  * @author Robin Collier
- *
+ * 
  */
-public enum DateFormatType
-{
-	XEP_0082_DATE_PROFILE("yyyy-MM-dd"), 
-	XEP_0082_DATETIME_PROFILE("yyyy-MM-dd'T'HH:mm:ssZ"),
-	XEP_0082_DATETIME_MILLIS_PROFILE("yyyy-MM-dd'T'HH:mm:ss.SSSZ"),
-	XEP_0082_TIME_PROFILE("hh:mm:ss"), 
-	XEP_0082_TIME_ZONE_PROFILE("hh:mm:ssZ"), 
-	XEP_0082_TIME_MILLIS_PROFILE("hh:mm:ss.SSS"), 
-	XEP_0082_TIME_MILLIS_ZONE_PROFILE("hh:mm:ss.SSSZ"),
-	XEP_0091_DATETIME("yyyyMMdd'T'HH:mm:ss");
-	
-	private String formatString;
-	
-	private DateFormatType(String dateFormat)
-	{
-		formatString = dateFormat;
-	}
-	
-	/**
-	 * Get the format string as defined in either XEP-0082 or XEP-0091.
-	 * @return The defined string format for the date.
-	 */
-	public String getFormatString()
-	{
-		return formatString;
-	}
-	
-	/**
-	 * Create a {@link SimpleDateFormat} object with the format defined by {@link #getFormatString()}.
-	 * @return A new date formatter.
-	 */
-	public SimpleDateFormat createFormatter()
-	{
-		return new SimpleDateFormat(getFormatString());
-	}
+public enum DateFormatType {
+    // @formatter:off
+    XEP_0082_DATE_PROFILE("yyyy-MM-dd"), 
+    XEP_0082_DATETIME_PROFILE("yyyy-MM-dd'T'HH:mm:ssZ"), 
+    XEP_0082_DATETIME_MILLIS_PROFILE("yyyy-MM-dd'T'HH:mm:ss.SSSZ"), 
+    XEP_0082_TIME_PROFILE("hh:mm:ss"), 
+    XEP_0082_TIME_ZONE_PROFILE("hh:mm:ssZ"), 
+    XEP_0082_TIME_MILLIS_PROFILE("hh:mm:ss.SSS"), 
+    XEP_0082_TIME_MILLIS_ZONE_PROFILE("hh:mm:ss.SSSZ"), 
+    XEP_0091_DATETIME("yyyyMMdd'T'HH:mm:ss");
+    // @formatter:on
+
+    private String formatString;
+
+    private DateFormatType(String dateFormat) {
+        formatString = dateFormat;
+    }
+
+    /**
+     * Get the format string as defined in either XEP-0082 or XEP-0091.
+     * 
+     * @return The defined string format for the date.
+     */
+    public String getFormatString() {
+        return formatString;
+    }
+
+    /**
+     * Create a {@link SimpleDateFormat} object with the format defined by {@link #getFormatString()}.
+     * 
+     * @return A new date formatter.
+     */
+    public SimpleDateFormat createFormatter() {
+        return new SimpleDateFormat(getFormatString());
+    }
 }
--- test-unit/org/jivesoftware/smack/TestUtils.java
+++ test-unit/org/jivesoftware/smack/TestUtils.java
@@ -3,7 +3,7 @@
  * $Revision$
  * $Date$
  *
- * Copyright 2003-2007 Jive Software.
+ * Copyright 2013 Robin Collier
  *
  * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,52 +26,43 @@ import org.xmlpull.mxp1.MXParser;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
-final public class TestUtils
-{
-	private TestUtils() {}
-	
-	public static XmlPullParser getIQParser(String stanza)
-	{
-		return getParser(stanza, "iq");
-	}
+final public class TestUtils {
+    private TestUtils() {
+    }
 
-	public static XmlPullParser getMessageParser(String stanza)
-	{
-		return getParser(stanza, "message");
-	}
+    public static XmlPullParser getIQParser(String stanza) {
+        return getParser(stanza, "iq");
+    }
 
-	public static XmlPullParser getPresenceParser(String stanza)
-	{
-		return getParser(stanza, "presence");
-	}
-	
-	public static XmlPullParser getParser(String stanza, String startTag)
-	{
-		XmlPullParser parser = new MXParser();
-		try
-		{
+    public static XmlPullParser getMessageParser(String stanza) {
+        return getParser(stanza, "message");
+    }
+
+    public static XmlPullParser getPresenceParser(String stanza) {
+        return getParser(stanza, "presence");
+    }
+
+    public static XmlPullParser getParser(String stanza, String startTag) {
+        XmlPullParser parser = new MXParser();
+        try {
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true);
-			parser.setInput(new StringReader(stanza));
-			boolean found = false;
+            parser.setInput(new StringReader(stanza));
+            boolean found = false;
 
-			while (!found)
-			{
-				if ((parser.next() == XmlPullParser.START_TAG) && parser.getName().equals(startTag))
-					found = true;
-			}
-			
-			if (!found)
-				throw new IllegalArgumentException("Cannot parse start tag [" + startTag + "] from stanze [" + stanza + "]");
-		}
-		catch (XmlPullParserException e)
-		{
-			throw new RuntimeException(e);
-		}
-		catch (IOException e)
-		{
-			throw new RuntimeException(e);
-		}
-		return parser;
-	}
+            while (!found) {
+                if ((parser.next() == XmlPullParser.START_TAG) && parser.getName().equals(startTag))
+                    found = true;
+            }
+
+            if (!found)
+                throw new IllegalArgumentException("Cannot parse start tag [" + startTag + "] from stanza [" + stanza
+                        + "]");
+        } catch (XmlPullParserException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return parser;
+    }
 
 }
