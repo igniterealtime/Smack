385cfac9fa3dcf53ad17857d10937b306ba3602c
Florian Schmaus <flo@geekplace.eu>
2013-10-31 20:28:40 +0000

    SMACK-459 Make it user proof by returning an unmodifiable set again
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@13797 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smackx/ServiceDiscoveryManager.java
+++ source/org/jivesoftware/smackx/ServiceDiscoveryManager.java
@@ -190,7 +190,7 @@ public class ServiceDiscoveryManager {
         Set<Identity> res = new HashSet<Identity>(identities);
         // Add the default identity that must exist
         res.add(defaultIdentity);
-        return res;
+        return Collections.unmodifiableSet(res);
     }
 
     /**
