2789591a5b13bf2dc807f0d19b25247258214c89
rcollier <rcollier@b35dd754-fafc-0310-a699-88a17e54d16e>
2014-01-22 02:25:32 +0000

    SMACK-403 Added support for Stanza forwarding.
    Code submitted by George Lukas...no, not that one.
    
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/branches/smack_3_4_0@13871 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ source/org/jivesoftware/smackx/forward/Forwarded.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2013 Georg Lukas
+ *
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jivesoftware.smackx.forward;
+
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.PacketExtension;
+import org.jivesoftware.smack.provider.PacketExtensionProvider;
+import org.jivesoftware.smack.util.PacketParserUtils;
+import org.jivesoftware.smackx.packet.DelayInfo;
+import org.jivesoftware.smackx.provider.DelayInfoProvider;
+import org.xmlpull.v1.XmlPullParser;
+
+/**
+ * Packet extension for <a href="http://xmpp.org/extensions/xep-0297.html">XEP-0297</a>: Stanza Forwarding.
+ * 
+ * @author Georg Lukas
+ */
+public class Forwarded implements PacketExtension {
+    public static final String NAMESPACE = "urn:xmpp:forward:0";
+    public static final String ELEMENT_NAME = "forwarded";
+
+    private DelayInfo delay;
+    private Packet forwardedPacket;
+
+    /**
+     * Creates a new Forwarded packet extension.
+     *
+     * @param delay an optional {@link DelayInfo} timestamp of the packet.
+     * @param fwdPacket the packet that is forwarded (required).
+     */
+    public Forwarded(DelayInfo delay, Packet fwdPacket) {
+        this.delay = delay;
+        this.forwardedPacket = fwdPacket;
+    }
+
+    @Override
+    public String getElementName() {
+        return ELEMENT_NAME;
+    }
+
+    @Override
+    public String getNamespace() {
+        return NAMESPACE;
+    }
+
+    @Override
+    public String toXML() {
+        StringBuilder buf = new StringBuilder();
+        buf.append("<").append(getElementName()).append(" xmlns=\"")
+                .append(getNamespace()).append("\">");
+
+        if (delay != null)
+            buf.append(delay.toXML());
+        buf.append(forwardedPacket.toXML());
+
+        buf.append("</").append(getElementName()).append(">");
+        return buf.toString();
+    }
+
+    /**
+     * get the packet forwarded by this stanza.
+     *
+     * @return the {@link Packet} instance (typically a message) that was forwarded.
+     */
+    public Packet getForwardedPacket() {
+        return forwardedPacket;
+    }
+
+    /**
+     * get the timestamp of the forwarded packet.
+     *
+     * @return the {@link DelayInfo} representing the time when the original packet was sent. May be null.
+     */
+    public DelayInfo getDelayInfo() {
+        return delay;
+    }
+}
---
+++ test-unit/org/jivesoftware/smackx/forward/ForwardedTest.java
@@ -0,0 +1,86 @@
+/**
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.forward;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+import java.util.TimeZone;
+
+import org.jivesoftware.smack.TestUtils;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smackx.packet.DelayInfo;
+import org.jivesoftware.smackx.packet.DelayInformation;
+import org.jivesoftware.smackx.forward.Forwarded;
+import org.jivesoftware.smackx.forward.provider.ForwardedProvider;
+import org.junit.Test;
+import org.xmlpull.mxp1.MXParser;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import com.jamesmurty.utils.XMLBuilder;
+
+public class ForwardedTest {
+
+    private static Properties outputProperties = new Properties();
+    static {
+        outputProperties.put(javax.xml.transform.OutputKeys.OMIT_XML_DECLARATION, "yes");
+    }
+
+    @Test
+    public void forwardedTest() throws Exception {
+        XmlPullParser parser;
+        String control;
+        Forwarded fwd;
+        
+        control = XMLBuilder.create("forwarded")
+            .a("xmlns", "urn:xmpp:forwarded:0")
+            .e("message")
+                    .a("from", "romeo@montague.com")
+            .asString(outputProperties);
+        
+        parser = TestUtils.getParser(control, "forwarded");
+        fwd = (Forwarded) new ForwardedProvider().parseExtension(parser);
+
+        // no delay in packet
+        assertEquals(null, fwd.getDelayInfo());
+        
+        // check message
+        assertEquals("romeo@montague.com", fwd.getForwardedPacket().getFrom());
+
+        // check end of tag
+        assertEquals(XmlPullParser.END_TAG, parser.getEventType());
+        assertEquals("forwarded", parser.getName());
+
+    }
+
+    @Test(expected=Exception.class)
+    public void forwardedEmptyTest() throws Exception {
+        XmlPullParser parser;
+        String control;
+        
+        control = XMLBuilder.create("forwarded")
+            .a("xmlns", "urn:xmpp:forwarded:0")
+            .asString(outputProperties);
+        
+        parser = TestUtils.getParser(control, "forwarded");
+        new ForwardedProvider().parseExtension(parser);
+    }
+}
