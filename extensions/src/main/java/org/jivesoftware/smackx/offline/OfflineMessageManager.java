igniterealtime/Smack

commit 7bd7b3d24cb30ef22dd4cdcf6b0f9b08d9cc5c4b

Improve packet send and result collecting API
Instead of repeating the same pattern, when sending an IQ get/set packet
and collecting the response

PacketFilter filter = new PacketIDFilter(request.getPacketID()),
PacketCollector collector = connection.createPacketCollector(filter);
connection.sendPacket(reg);
IQ result = (IQ)collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
// Stop queuing results
collector.cancel();
if (result == null) {
    throw new XMPPException("No response from server.");
}
else if (result.getType() == IQ.Type.ERROR) {
    throw new XMPPException(result.getError());
}

the API got redesigned, so that the above code block can be replaced
with

Packet result = connection.createPacketCollectorAndSend(request).nextResultOrThrow();

extensions/src/main/java/org/jivesoftware/smackx/offline/OfflineMessageManager.java
@@ -21,8 +21,10 @@ import org.jivesoftware.smack.PacketCollector;
 import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.filter.*;
-import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.filter.AndFilter;
+import org.jivesoftware.smack.filter.PacketExtensionFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.PacketTypeFilter;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
@@ -139,9 +141,8 @@ public class OfflineMessageManager {
             item.setAction("view");
             request.addItem(item);
         }
-        // Filter packets looking for an answer from the server.
-        PacketFilter responseFilter = new PacketIDFilter(request.getPacketID());
-        PacketCollector response = connection.createPacketCollector(responseFilter);
+        connection.createPacketCollectorAndSend(request).nextResultOrThrow();
+
         // Filter offline messages that were requested by this request
         PacketFilter messageFilter = new AndFilter(packetFilter, new PacketFilter() {
             public boolean accept(Packet packet) {
@@ -151,27 +152,13 @@ public class OfflineMessageManager {
             }
         });
         PacketCollector messageCollector = connection.createPacketCollector(messageFilter);
-        // Send the retrieval request to the server.
-        connection.sendPacket(request);
-        // Wait up to a certain number of seconds for a reply.
-        IQ answer = (IQ) response.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        response.cancel();
-
-        if (answer == null) {
-            throw new XMPPException("No response from server.");
-        } else if (answer.getError() != null) {
-            throw new XMPPException(answer.getError());
-        }
-
         // Collect the received offline messages
-        Message message = (Message) messageCollector.nextResult(
-                SmackConfiguration.getPacketReplyTimeout());
+        Message message = (Message) messageCollector.nextResult();
         while (message != null) {
             messages.add(message);
             message =
                     (Message) messageCollector.nextResult(
-                            SmackConfiguration.getPacketReplyTimeout());
+                            SmackConfiguration.getDefaultPacketReplyTimeout());
         }
         // Stop queuing offline messages
         messageCollector.cancel();
@@ -191,32 +178,16 @@ public class OfflineMessageManager {
         List<Message> messages = new ArrayList<Message>();
         OfflineMessageRequest request = new OfflineMessageRequest();
         request.setFetch(true);
-        // Filter packets looking for an answer from the server.
-        PacketFilter responseFilter = new PacketIDFilter(request.getPacketID());
-        PacketCollector response = connection.createPacketCollector(responseFilter);
-        // Filter offline messages that were requested by this request
+        connection.createPacketCollectorAndSend(request).nextResultOrThrow();
+
         PacketCollector messageCollector = connection.createPacketCollector(packetFilter);
-        // Send the retrieval request to the server.
-        connection.sendPacket(request);
-        // Wait up to a certain number of seconds for a reply.
-        IQ answer = (IQ) response.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        response.cancel();
-
-        if (answer == null) {
-            throw new XMPPException("No response from server.");
-        } else if (answer.getError() != null) {
-            throw new XMPPException(answer.getError());
-        }
-
         // Collect the received offline messages
-        Message message = (Message) messageCollector.nextResult(
-                SmackConfiguration.getPacketReplyTimeout());
+        Message message = (Message) messageCollector.nextResult();
         while (message != null) {
             messages.add(message);
             message =
                     (Message) messageCollector.nextResult(
-                            SmackConfiguration.getPacketReplyTimeout());
+                            SmackConfiguration.getDefaultPacketReplyTimeout());
         }
         // Stop queuing offline messages
         messageCollector.cancel();
@@ -238,21 +209,7 @@ public class OfflineMessageManager {
             item.setAction("remove");
             request.addItem(item);
         }
-        // Filter packets looking for an answer from the server.
-        PacketFilter responseFilter = new PacketIDFilter(request.getPacketID());
-        PacketCollector response = connection.createPacketCollector(responseFilter);
-        // Send the deletion request to the server.
-        connection.sendPacket(request);
-        // Wait up to a certain number of seconds for a reply.
-        IQ answer = (IQ) response.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        response.cancel();
-
-        if (answer == null) {
-            throw new XMPPException("No response from server.");
-        } else if (answer.getError() != null) {
-            throw new XMPPException(answer.getError());
-        }
+        connection.createPacketCollectorAndSend(request).nextResultOrThrow();
     }
 
     /**
@@ -264,20 +221,6 @@ public class OfflineMessageManager {
     public void deleteMessages() throws XMPPException {
         OfflineMessageRequest request = new OfflineMessageRequest();
         request.setPurge(true);
-        // Filter packets looking for an answer from the server.
-        PacketFilter responseFilter = new PacketIDFilter(request.getPacketID());
-        PacketCollector response = connection.createPacketCollector(responseFilter);
-        // Send the deletion request to the server.
-        connection.sendPacket(request);
-        // Wait up to a certain number of seconds for a reply.
-        IQ answer = (IQ) response.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        response.cancel();
-
-        if (answer == null) {
-            throw new XMPPException("No response from server.");
-        } else if (answer.getError() != null) {
-            throw new XMPPException(answer.getError());
-        }
+        connection.createPacketCollectorAndSend(request).nextResultOrThrow();
     }
 }

core/src/main/java/org/jivesoftware/smack/AccountManager.java
@@ -227,19 +227,7 @@ public class AccountManager {
         attributes.put("username",username);
         attributes.put("password",password);
         reg.setAttributes(attributes);
-        PacketFilter filter = new AndFilter(new PacketIDFilter(reg.getPacketID()),
-                new PacketTypeFilter(IQ.class));
-        PacketCollector collector = connection.createPacketCollector(filter);
-        connection.sendPacket(reg);
-        IQ result = (IQ)collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        collector.cancel();
-        if (result == null) {
-            throw new XMPPException("No response from server.");
-        }
-        else if (result.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(result.getError());
-        }
+        connection.createPacketCollectorAndSend(reg).nextResultOrThrow();
     }
 
     /**
@@ -262,15 +250,7 @@ public class AccountManager {
                 new PacketTypeFilter(IQ.class));
         PacketCollector collector = connection.createPacketCollector(filter);
         connection.sendPacket(reg);
-        IQ result = (IQ)collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        collector.cancel();
-        if (result == null) {
-            throw new XMPPException("No response from server.");
-        }
-        else if (result.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(result.getError());
-        }
+        collector.nextResultOrThrow();
     }
 
     /**
@@ -292,19 +272,7 @@ public class AccountManager {
         // To delete an account, we add a single attribute, "remove", that is blank.
         attributes.put("remove", "");
         reg.setAttributes(attributes);
-        PacketFilter filter = new AndFilter(new PacketIDFilter(reg.getPacketID()),
-                new PacketTypeFilter(IQ.class));
-        PacketCollector collector = connection.createPacketCollector(filter);
-        connection.sendPacket(reg);
-        IQ result = (IQ)collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        collector.cancel();
-        if (result == null) {
-            throw new XMPPException("No response from server.");
-        }
-        else if (result.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(result.getError());
-        }
+        connection.createPacketCollectorAndSend(reg).nextResultOrThrow();
     }
 
     /**
@@ -315,21 +283,6 @@ public class AccountManager {
     private synchronized void getRegistrationInfo() throws XMPPException {
         Registration reg = new Registration();
         reg.setTo(connection.getServiceName());
-        PacketFilter filter = new AndFilter(new PacketIDFilter(reg.getPacketID()),
-                new PacketTypeFilter(IQ.class));
-        PacketCollector collector = connection.createPacketCollector(filter);
-        connection.sendPacket(reg);
-        IQ result = (IQ)collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        // Stop queuing results
-        collector.cancel();
-        if (result == null) {
-            throw new XMPPException("No response from server.");
-        }
-        else if (result.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(result.getError());
-        }
-        else {
-            info = (Registration)result;
-        }
+        info = (Registration) connection.createPacketCollectorAndSend(reg).nextResultOrThrow();
     }
 }

core/src/main/java/org/jivesoftware/smack/Connection.java
@@ -37,6 +37,7 @@ import org.jivesoftware.smack.compression.XMPPInputOutputStream;
 import org.jivesoftware.smack.compression.Java7ZlibInputOutputStream;
 import org.jivesoftware.smack.debugger.SmackDebugger;
 import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.PacketIDFilter;
 import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smack.packet.Presence;
 
@@ -171,6 +172,11 @@ public abstract class Connection {
      * The ChatManager keeps track of references to all current chats.
      */
     private ChatManager chatManager = null;
+
+    /**
+     * 
+     */
+    private long packetReplyTimeout = SmackConfiguration.getDefaultPacketReplyTimeout();
 
     /**
      * The SmackDebugger allows to log and debug XML traffic.
@@ -564,6 +570,24 @@ public abstract class Connection {
     }
 
     /**
+     * Creates a new packet collector collecting packets that are replies to <code>packet</code>.
+     * Does also send <code>packet</code>. Note that the packet filter is at the moment created as
+     * ID filter of <code>packet</code>'s ID. This may change in the future to also check the
+     * correct "from JID" value.
+     * 
+     * @param packet the packet to filter responses from
+     * @return a new packet collector.
+     */
+    public PacketCollector createPacketCollectorAndSend(Packet packet) {
+        PacketFilter packetFilter = new PacketIDFilter(packet.getPacketID());
+        // Create the packet collector before sending the packet
+        PacketCollector packetCollector = createPacketCollector(packetFilter);
+        // Now we can send the packet as the collector has been created
+        sendPacket(packet);
+        return packetCollector;
+    }
+
+    /**
      * Creates a new packet collector for this connection. A packet filter determines
      * which packets will be accumulated by the collector. A PacketCollector is
      * more suitable to use than a {@link PacketListener} when you need to wait for
@@ -846,6 +870,10 @@ public abstract class Connection {
      */
     protected void setRosterVersioningSupported() {
         rosterVersioningSupported = true;
+    }
+
+    public long getPacketReplyTimeout() {
+        return packetReplyTimeout;
     }
 
     /**

core/src/main/java/org/jivesoftware/smack/NonSASLAuthentication.java
@@ -17,9 +17,9 @@
 
 package org.jivesoftware.smack;
 
-import org.jivesoftware.smack.filter.PacketIDFilter;
 import org.jivesoftware.smack.packet.Authentication;
 import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.packet.Packet;
 
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.PasswordCallback;
@@ -60,22 +60,9 @@ class NonSASLAuthentication implements UserAuthentication {
         discoveryAuth.setType(IQ.Type.GET);
         discoveryAuth.setUsername(username);
 
-        PacketCollector collector =
-            connection.createPacketCollector(new PacketIDFilter(discoveryAuth.getPacketID()));
-        // Send the packet
-        connection.sendPacket(discoveryAuth);
-        // Wait up to a certain number of seconds for a response from the server.
-        IQ response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        if (response == null) {
-            throw new XMPPException("No response from the server.");
-        }
-        // If the server replied with an error, throw an exception.
-        else if (response.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(response.getError());
-        }
         // Otherwise, no error so continue processing.
-        Authentication authTypes = (Authentication) response;
-        collector.cancel();
+        Authentication authTypes = (Authentication) connection.createPacketCollectorAndSend(
+                        discoveryAuth).nextResultOrThrow();
 
         // Now, create the authentication packet we'll send to the server.
         Authentication auth = new Authentication();
@@ -94,19 +81,7 @@ class NonSASLAuthentication implements UserAuthentication {
 
         auth.setResource(resource);
 
-        collector = connection.createPacketCollector(new PacketIDFilter(auth.getPacketID()));
-        // Send the packet.
-        connection.sendPacket(auth);
-        // Wait up to a certain number of seconds for a response from the server.
-        response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        if (response == null) {
-            throw new XMPPException("Authentication failed.");
-        }
-        else if (response.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(response.getError());
-        }
-        // We're done with the collector, so explicitly cancel it.
-        collector.cancel();
+        Packet response = connection.createPacketCollectorAndSend(auth).nextResultOrThrow();
 
         return response.getTo();
     }
@@ -115,20 +90,7 @@ class NonSASLAuthentication implements UserAuthentication {
         // Create the authentication packet we'll send to the server.
         Authentication auth = new Authentication();
 
-        PacketCollector collector =
-            connection.createPacketCollector(new PacketIDFilter(auth.getPacketID()));
-        // Send the packet.
-        connection.sendPacket(auth);
-        // Wait up to a certain number of seconds for a response from the server.
-        IQ response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        if (response == null) {
-            throw new XMPPException("Anonymous login failed.");
-        }
-        else if (response.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(response.getError());
-        }
-        // We're done with the collector, so explicitly cancel it.
-        collector.cancel();
+        Packet response = connection.createPacketCollectorAndSend(auth).nextResultOrThrow();
 
         if (response.getTo() != null) {
             return response.getTo();

core/src/main/java/org/jivesoftware/smack/PacketCollector.java
@@ -22,6 +22,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.jivesoftware.smack.filter.PacketFilter;
 import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.XMPPError;
 
 /**
  * Provides a mechanism to collect packets into a result queue that pass a
@@ -105,26 +106,35 @@ public class PacketCollector {
     }
 
     /**
-     * Returns the next available packet. The method call will block (not return)
-     * until a packet is available.
-     *
+     * Returns the next available packet. The method call will block (not return) until a packet is
+     * available.
+     * 
      * @return the next available packet.
      */
-    public Packet nextResult() {
+    public Packet nextResultBlockForever() {
         try {
-			return resultQueue.take();
-		}
-		catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
+            return resultQueue.take();
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns the next available packet. The method call will block until the connection's default
+     * timeout has elapsed.
+     * 
+     * @return the next availabe packet.
+     */
+    public Packet nextResult() {
+        return nextResult(connection.getPacketReplyTimeout());
     }
 
     /**
      * Returns the next available packet. The method call will block (not return)
-     * until a packet is available or the <tt>timeout</tt> has elapased. If the
+     * until a packet is available or the <tt>timeout</tt> has elapsed. If the
      * timeout elapses without a result, <tt>null</tt> will be returned.
      *
-     * @param timeout the amount of time to wait for the next packet (in milleseconds).
      * @return the next available packet.
      */
     public Packet nextResult(long timeout) {
@@ -134,6 +144,41 @@ public class PacketCollector {
 		catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
+    }
+
+    /**
+     * Returns the next available packet. The method call will block until a packet is available or
+     * the connections reply timeout has elapsed. If the timeout elapses without a result,
+     * <tt>null</tt> will be returned. This method does also cancel the PacketCollector.
+     * 
+     * @return the next available packet.
+     * @throws XMPPException
+     */
+    public Packet nextResultOrThrow() throws XMPPException {
+        return nextResultOrThrow(connection.getPacketReplyTimeout());
+    }
+
+    /**
+     * Returns the next available packet. The method call will block until a packet is available or
+     * the <tt>timeout</tt> has elapsed. This method does also cancel the PacketCollector.
+     * 
+     * @param timeout the amount of time to wait for the next packet (in milleseconds).
+     * @return the next available packet.
+     * @throws XMPPException in case of no response or error response
+     */
+    public Packet nextResultOrThrow(long timeout) throws XMPPException {
+        Packet result = nextResult(timeout);
+        cancel();
+        if (result == null) {
+            throw new XMPPException(SmackError.NO_RESPONSE_FROM_SERVER);
+        }
+
+        XMPPError xmppError = result.getError();
+        if (xmppError != null) {
+            throw new XMPPException(xmppError);
+        }
+
+        return result;
     }
 
     /**

core/src/main/java/org/jivesoftware/smack/PacketReader.java
@@ -108,7 +108,7 @@ class PacketReader {
             // (although this is a rare thing). Therefore, we continue waiting
             // until either a connectionID has been set (and hence a notify was
             // made) or the total wait time has elapsed.
-            int waitTime = SmackConfiguration.getPacketReplyTimeout();
+            int waitTime = SmackConfiguration.getDefaultPacketReplyTimeout();
             wait(3 * waitTime);
         }
         catch (InterruptedException ie) {

core/src/main/java/org/jivesoftware/smack/Roster.java
@@ -277,19 +277,7 @@ public class Roster {
             }
         }
         rosterPacket.addRosterItem(item);
-        // Wait up to a certain number of seconds for a reply from the server.
-        PacketCollector collector = connection.createPacketCollector(
-                new PacketIDFilter(rosterPacket.getPacketID()));
-        connection.sendPacket(rosterPacket);
-        IQ response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        collector.cancel();
-        if (response == null) {
-            throw new XMPPException("No response from the server.");
-        }
-        // If the server replied with an error, throw an exception.
-        else if (response.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(response.getError());
-        }
+        connection.createPacketCollectorAndSend(rosterPacket).nextResultOrThrow();
 
         // Create a presence subscription packet and send.
         Presence presencePacket = new Presence(Presence.Type.subscribe);
@@ -326,18 +314,7 @@ public class Roster {
         // Set the item type as REMOVE so that the server will delete the entry
         item.setItemType(RosterPacket.ItemType.remove);
         packet.addRosterItem(item);
-        PacketCollector collector = connection.createPacketCollector(
-                new PacketIDFilter(packet.getPacketID()));
-        connection.sendPacket(packet);
-        IQ response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-        collector.cancel();
-        if (response == null) {
-            throw new XMPPException("No response from the server.");
-        }
-        // If the server replied with an error, throw an exception.
-        else if (response.getType() == IQ.Type.ERROR) {
-            throw new XMPPException(response.getError());
-        }
+        connection.createPacketCollectorAndSend(packet).nextResultOrThrow();
     }
 
     /**

core/src/main/java/org/jivesoftware/smack/RosterGroup.java
@@ -178,15 +178,7 @@ public class RosterGroup {
             }
         }
         if (collector != null) {
-            IQ response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-            collector.cancel();
-            if (response == null) {
-                throw new XMPPException("No response from the server.");
-            }
-            // If the server replied with an error, throw an exception.
-            else if (response.getType() == IQ.Type.ERROR) {
-                throw new XMPPException(response.getError());
-            }
+            collector.nextResultOrThrow();
         }
     }
 
@@ -220,15 +212,7 @@ public class RosterGroup {
             }
         }
         if (collector != null) {
-            IQ response = (IQ) collector.nextResult(SmackConfiguration.getPacketReplyTimeout());
-            collector.cancel();
-            if (response == null) {
-                throw new XMPPException("No response from the server.");
-            }
-            // If the server replied with an error, throw an exception.
-            else if (response.getType() == IQ.Type.ERROR) {
-                throw new XMPPException(response.getError());
-            }
+            collector.nextResultOrThrow();
         }
     }
 

commit 2c7f1efe80b988ba027d9ac89eee3da3c9d3f844

Send request after the PacketCollector was setup
This bug was introduced with 7bd7b3d24. Thanks to Lars Noschinski for
discovering it.

extensions/src/main/java/org/jivesoftware/smackx/offline/OfflineMessageManager.java
@@ -141,8 +141,6 @@ public class OfflineMessageManager {
             item.setAction("view");
             request.addItem(item);
         }
-        connection.createPacketCollectorAndSend(request).nextResultOrThrow();
-
         // Filter offline messages that were requested by this request
         PacketFilter messageFilter = new AndFilter(packetFilter, new PacketFilter() {
             public boolean accept(Packet packet) {
@@ -152,6 +150,7 @@ public class OfflineMessageManager {
             }
         });
         PacketCollector messageCollector = connection.createPacketCollector(messageFilter);
+        connection.createPacketCollectorAndSend(request).nextResultOrThrow();
         // Collect the received offline messages
         Message message = (Message) messageCollector.nextResult();
         while (message != null) {