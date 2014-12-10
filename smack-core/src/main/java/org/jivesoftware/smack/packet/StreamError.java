923395eeba3b6ef88892eecf832e1a66bae7082d
Jae Jang <jae.jang@gmail.com>
2014-12-10 02:49:38 -0500

    Remove String isEmpty to support Java 1.5

--- smack-core/src/main/java/org/jivesoftware/smack/packet/StreamError.java
+++ smack-core/src/main/java/org/jivesoftware/smack/packet/StreamError.java
@@ -105,7 +105,7 @@ public class StreamError extends AbstractError implements PlainStreamElement {
 
     public StreamError(Condition condition, String conditionText, Map<String, String> descriptiveTexts, List<PacketExtension> extensions) {
         super(descriptiveTexts, extensions);
-        if (conditionText != null && !conditionText.isEmpty()) {
+        if (conditionText != null && !"".equals(conditionText)) {
             switch (condition) {
             case see_other_host:
                 break;
