e5c6c9bdf8b2fe665f6b13d8d624030735f9a964
Florian Schmaus <flo@geekplace.eu>
2015-01-06 18:39:22 +0100

    Remove unused private field in Session

--- smack-core/src/main/java/org/jivesoftware/smack/packet/Session.java
+++ smack-core/src/main/java/org/jivesoftware/smack/packet/Session.java
@@ -37,8 +37,6 @@ public class Session extends SimpleIQ {
     public static final String ELEMENT = "session";
     public static final String NAMESPACE = "urn:ietf:params:xml:ns:xmpp-session";
 
-    private static final String SESSION = '<' + ELEMENT + " xmlns='" + NAMESPACE + "'/>";
-
     public Session() {
         super(ELEMENT, NAMESPACE);
         setType(IQ.Type.set);
