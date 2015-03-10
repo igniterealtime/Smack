d389b4db201c446fca44c69f5594dfd78c0cc769
Florian Schmaus <flo@geekplace.eu>
2015-03-10 13:21:16 +0100

    Fix NPE in EntityCapsManager
    persistentCache may be null.

--- smack-extensions/src/main/java/org/jivesoftware/smackx/caps/EntityCapsManager.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/caps/EntityCapsManager.java
@@ -199,7 +199,7 @@ public class EntityCapsManager extends Manager {
         DiscoverInfo info = CAPS_CACHE.get(nodeVer);
 
         // If it was not in CAPS_CACHE, try to retrieve the information from persistentCache
-        if (info == null) {
+        if (info == null && persistentCache != null) {
             info = persistentCache.lookup(nodeVer);
             // Promote the information to CAPS_CACHE if one was found
             if (info != null) {
