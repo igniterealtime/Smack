17c96612839e96c980f8fa79407935af470127eb
Florian Schmaus <flo@geekplace.eu>
2016-07-01 08:18:56 +0200

    Handle AlreadyLoggedInException in ReconnectionManager
    Fixes SMACK-725.

--- smack-core/src/main/java/org/jivesoftware/smack/ReconnectionManager.java
+++ smack-core/src/main/java/org/jivesoftware/smack/ReconnectionManager.java
@@ -250,6 +250,12 @@ public final class ReconnectionManager {
                         // Successfully reconnected.
                         attempts = 0;
                     }
+                    catch (SmackException.AlreadyLoggedInException e) {
+                        // This can happen if another thread concurrently triggers a reconnection
+                        // and/or login. Obviously it should not be handled as a reconnection
+                        // failure. See also SMACK-725.
+                        LOGGER.log(Level.FINER, "Reconnection not required, was already logged in", e);
+                    }
                     catch (SmackException | IOException | XMPPException | InterruptedException e) {
                         // Fires the failed reconnection notification
                         for (ConnectionListener listener : connection.connectionListeners) {
