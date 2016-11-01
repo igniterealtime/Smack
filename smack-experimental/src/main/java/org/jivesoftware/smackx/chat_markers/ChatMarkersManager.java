6d74d0383c106da255588973db67b1351dda3351
Fernando Ramirez <f.e.ramirez94@gmail.com>
2016-11-01 13:51:04 -0300

    Add support for XEP-0333: Chat Markers
    Fixes SMACK-736

---
+++ smack-experimental/src/main/java/org/jivesoftware/smackx/chat_markers/ChatMarkersManager.java
@@ -0,0 +1,89 @@
+/**
+ *
+ * Copyright © 2016 Fernando Ramirez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.chat_markers;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import org.jivesoftware.smack.ConnectionCreationListener;
+import org.jivesoftware.smack.Manager;
+import org.jivesoftware.smack.SmackException.NoResponseException;
+import org.jivesoftware.smack.SmackException.NotConnectedException;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPConnectionRegistry;
+import org.jivesoftware.smack.XMPPException.XMPPErrorException;
+import org.jivesoftware.smackx.chat_markers.element.ChatMarkersElements;
+import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
+
+/**
+ * Chat Markers Manager class (XEP-0333).
+ * 
+ * @see <a href="http://xmpp.org/extensions/xep-0333.html">XEP-0333: Chat
+ *      Markers</a>
+ * @author Fernando Ramirez
+ * 
+ */
+public final class ChatMarkersManager extends Manager {
+
+    static {
+        XMPPConnectionRegistry.addConnectionCreationListener(new ConnectionCreationListener() {
+            @Override
+            public void connectionCreated(XMPPConnection connection) {
+                getInstanceFor(connection);
+            }
+        });
+    }
+
+    private static final Map<XMPPConnection, ChatMarkersManager> INSTANCES = new WeakHashMap<>();
+
+    /**
+     * Get the singleton instance of ChatMarkersManager.
+     * 
+     * @param connection
+     * @return the instance of ChatMarkersManager
+     */
+    public static synchronized ChatMarkersManager getInstanceFor(XMPPConnection connection) {
+        ChatMarkersManager chatMarkersManager = INSTANCES.get(connection);
+
+        if (chatMarkersManager == null) {
+            chatMarkersManager = new ChatMarkersManager(connection);
+            INSTANCES.put(connection, chatMarkersManager);
+        }
+
+        return chatMarkersManager;
+    }
+
+    private ChatMarkersManager(XMPPConnection connection) {
+        super(connection);
+    }
+
+    /**
+     * Returns true if Chat Markers is supported by the server.
+     * 
+     * @return true if Chat Markers is supported by the server.
+     * @throws NotConnectedException
+     * @throws XMPPErrorException
+     * @throws NoResponseException
+     * @throws InterruptedException
+     */
+    public boolean isSupportedByServer()
+            throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        return ServiceDiscoveryManager.getInstanceFor(connection())
+                .serverSupportsFeature(ChatMarkersElements.NAMESPACE);
+    }
+
+}
---
+++ smack-experimental/src/test/java/org/jivesoftware/smackx/chat_markers/ReceivedExtensionTest.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * Copyright © 2016 Fernando Ramirez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.chat_markers;
+
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.util.PacketParserUtils;
+import org.jivesoftware.smackx.chat_markers.element.ChatMarkersElements;
+import org.jivesoftware.smackx.chat_markers.element.ChatMarkersElements.ReceivedExtension;
+import org.jivesoftware.smackx.chat_markers.provider.ReceivedProvider;
+import org.junit.Assert;
+import org.junit.Test;
+import org.jxmpp.jid.impl.JidCreate;
+import org.xmlpull.v1.XmlPullParser;
+
+public class ReceivedExtensionTest {
+
+    String receivedMessageStanza = "<message to='northumberland@shakespeare.lit/westminster' id='message-2'>"
+            + "<received xmlns='urn:xmpp:chat-markers:0' id='message-1'/>" + "</message>";
+
+    String receivedExtension = "<received xmlns='urn:xmpp:chat-markers:0' id='message-1'/>";
+
+    @Test
+    public void checkReceivedExtension() throws Exception {
+        Message message = new Message(JidCreate.from("northumberland@shakespeare.lit/westminster"));
+        message.setStanzaId("message-2");
+        message.addExtension(new ChatMarkersElements.ReceivedExtension("message-1"));
+        Assert.assertEquals(receivedMessageStanza, message.toXML().toString());
+    }
+
+    @Test
+    public void checkReceivedProvider() throws Exception {
+        XmlPullParser parser = PacketParserUtils.getParserFor(receivedExtension);
+        ReceivedExtension receivedExtension1 = new ReceivedProvider().parse(parser);
+        Assert.assertEquals("message-1", receivedExtension1.getId());
+
+        Message message = (Message) PacketParserUtils.parseStanza(receivedMessageStanza);
+        ReceivedExtension receivedExtension2 = ReceivedExtension.from(message);
+        Assert.assertEquals("message-1", receivedExtension2.getId());
+    }
+
+}
