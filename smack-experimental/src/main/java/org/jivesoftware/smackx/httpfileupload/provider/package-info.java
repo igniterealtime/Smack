72d4c8b611299e50922d23eab3f864cb11dceadc
Grigory Fedorov <gregory.230101@gmail.com>
2017-01-30 02:07:29 +0500

    Add support for XEP-0363: HTTP File Upload.
    Fixes SMACK-747

---
+++ smack-experimental/src/main/java/org/jivesoftware/smackx/httpfileupload/provider/package-info.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * Copyright © 2017 Grigory Fedorov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Providers for XEP-0363: HTTP File Upload.
+ *
+ * @author Grigory Fedorov
+ * @see <a href="http://xmpp.org/extensions/xep-0363.html">XEP-0363: HTTP File Upload</a>
+ */
+package org.jivesoftware.smackx.httpfileupload.provider;
