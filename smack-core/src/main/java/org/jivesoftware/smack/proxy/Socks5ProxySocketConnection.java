93683389e325621d5135d20bd267ecdaa2aa8fbb
lohse <candy.lohse@ivi.fraunhofer.de>
2018-01-19 11:38:17 +0100

    Bugfix in SOCKS5 authentication
    Read password bytes from the correct field.
    Fixes SMACK-796, introduced with 4c64643

--- smack-core/src/main/java/org/jivesoftware/smack/proxy/Socks5ProxySocketConnection.java
+++ smack-core/src/main/java/org/jivesoftware/smack/proxy/Socks5ProxySocketConnection.java
@@ -137,7 +137,7 @@ public class Socks5ProxySocketConnection implements ProxySocketConnection {
                     System.arraycopy(userBytes, 0, buf, index, 
                         user.length());
                     index += user.length();
-                    byte[] passwordBytes = user.getBytes(StringUtils.UTF8);
+                    byte[] passwordBytes = passwd.getBytes(StringUtils.UTF8);
                     buf[index++] = (byte) (passwordBytes.length);
                     System.arraycopy(passwordBytes, 0, buf, index, 
                         passwd.length());
