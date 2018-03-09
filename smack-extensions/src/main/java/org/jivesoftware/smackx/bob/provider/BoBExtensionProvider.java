67b879b3728c87d39424eb2f1133db9dd685e212
Jesus Fuentes <fuentes.jesus1010010@gmail.com>
2018-03-08 18:50:16 -0600

    Add Unit Test
    Add Unit Test (fails b/c namespace is null/empty)

--- smack-extensions/src/main/java/org/jivesoftware/smackx/bob/provider/BoBExtensionProvider.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/bob/provider/BoBExtensionProvider.java
@@ -1,6 +1,21 @@
+/**
+ *
+ * Copyright 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jivesoftware.smackx.bob.provider;
 
-import org.jivesoftware.smack.util.ParserUtils;
 import org.jivesoftware.smackx.bob.BoBHash;
 import org.jivesoftware.smackx.bob.element.BoBExtension;
 import org.jivesoftware.smackx.xhtmlim.XHTMLText;
@@ -27,8 +42,8 @@ public class BoBExtensionProvider extends XHTMLExtensionProvider {
                             paragraph = parser.nextText();
                             break;
                         case XHTMLText.IMG:
-                            alt = ParserUtils.getRequiredAttribute(parser, BoBExtension.ALT);
-                            src = ParserUtils.getRequiredAttribute(parser, BoBExtension.SRC);
+                            alt = parser.getAttributeValue(null, BoBExtension.ALT);
+                            src = parser.getAttributeValue(null, BoBExtension.SRC);
                     }
                     break;
                 case XmlPullParser.END_TAG:
--- smack-extensions/src/test/java/org/jivesoftware/smackx/bob/BoBExtensionProviderTest.java
+++ smack-extensions/src/test/java/org/jivesoftware/smackx/bob/BoBExtensionProviderTest.java
@@ -1,3 +1,19 @@
+/**
+ *
+ * Copyright 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jivesoftware.smackx.bob;
 
 import static org.junit.Assert.assertNotNull;
@@ -7,7 +23,7 @@ import org.jivesoftware.smack.test.util.TestUtils;
 import org.jivesoftware.smackx.bob.element.BoBExtension;
 import org.junit.Test;
 
-public class BoBExtenstionTest extends SmackTestSuite {
+public class BoBExtensionProviderTest extends SmackTestSuite {
 
     private static final String sampleBoBExtensionIM = "<message from='ladymacbeth@shakespeare.lit/castle' "
             + "to='macbeth@chat.shakespeare.lit' type='groupchat'>"
@@ -20,7 +36,7 @@ public class BoBExtenstionTest extends SmackTestSuite {
             + "</p></body></html></message>";
 
     @Test
-    public void checkBoBExtensionIM() throws Exception {
+    public void parseTest() throws Exception {
         BoBExtension boBExtension = TestUtils.parseExtensionElement(sampleBoBExtensionIM);
         assertNotNull(boBExtension);
     }
