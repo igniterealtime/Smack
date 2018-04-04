2a56b24929eb09dc97b6d421f5e2f38e92e9c6ad
Paul Schaub <vanitasvitae@fsfe.org>
2018-04-04 15:41:59 +0200

    Add IntegrationTests for chat2 API

---
+++ smack-integration-test/src/main/java/org/jivesoftware/smackx/chat/IncomingMessageListenerIntegrationTest.java
@@ -0,0 +1,63 @@
+/**
+ *
+ * Copyright 2018 Paul Schaub
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.chat;
+
+import org.jivesoftware.smack.chat2.Chat;
+import org.jivesoftware.smack.chat2.ChatManager;
+import org.jivesoftware.smack.chat2.IncomingChatMessageListener;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.util.StringUtils;
+
+import org.igniterealtime.smack.inttest.AbstractSmackIntegrationTest;
+import org.igniterealtime.smack.inttest.SmackIntegrationTest;
+import org.igniterealtime.smack.inttest.SmackIntegrationTestEnvironment;
+import org.igniterealtime.smack.inttest.util.SimpleResultSyncPoint;
+import org.jxmpp.jid.EntityBareJid;
+
+public class IncomingMessageListenerIntegrationTest extends AbstractSmackIntegrationTest {
+
+    public IncomingMessageListenerIntegrationTest(SmackIntegrationTestEnvironment environment) {
+        super(environment);
+    }
+
+    @SmackIntegrationTest
+    public void test() throws Exception {
+        ChatManager chatManagerOne = ChatManager.getInstanceFor(conOne);
+        ChatManager chatManagerTwo = ChatManager.getInstanceFor(conTwo);
+
+        final String body = StringUtils.randomString(16);
+        final SimpleResultSyncPoint syncPoint = new SimpleResultSyncPoint();
+        final IncomingChatMessageListener listener = new IncomingChatMessageListener() {
+            @Override
+            public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
+                if (body.equals(message.getBody())) {
+                    syncPoint.signal();
+                }
+            }
+        };
+
+        try {
+            chatManagerTwo.addIncomingListener(listener);
+            Chat chat = chatManagerOne.chatWith(conTwo.getUser().asEntityBareJid());
+            chat.send(body);
+            syncPoint.waitForResult(10 * 1000);
+        }
+        finally {
+            chatManagerTwo.removeIncomingListener(listener);
+        }
+    }
+}
