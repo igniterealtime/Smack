901b316c06b2a455461f43befbd937415b5cf3a6
Paul Schaub <vanitasvitae@fsfe.org>
2018-05-06 18:47:44 +0200

    Add and register BodyElementProvider

---
+++ smack-core/src/main/java/org/jivesoftware/smack/provider/BodyElementProvider.java
@@ -0,0 +1,38 @@
+/**
+ *
+ * Copyright 2018 Paul Schaub.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smack.provider;
+
+import static org.jivesoftware.smack.util.PacketParserUtils.parseElementText;
+
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.util.ParserUtils;
+
+import org.xmlpull.v1.XmlPullParser;
+
+public class BodyElementProvider extends ExtensionElementProvider<Message.Body> {
+
+    @Override
+    public Message.Body parse(XmlPullParser parser, int initialDepth) throws Exception {
+        String xmlLang = ParserUtils.getXmlLang(parser);
+        if (xmlLang == null) {
+            xmlLang = "en";
+        }
+
+        String body = parseElementText(parser);
+        return new Message.Body(xmlLang, body);
+    }
+}
