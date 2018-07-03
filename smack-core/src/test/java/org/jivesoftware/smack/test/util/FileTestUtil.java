4ee034bf4035eaa7282005de53b2596a39b513bc
Paul Schaub <vanitasvitae@fsfe.org>
2018-07-03 10:39:54 +0200

    Add utility method to get temp directory

---
+++ smack-core/src/test/java/org/jivesoftware/smack/test/util/FileTestUtil.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright © 2018 Paul Schaub
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smack.test.util;
+
+import java.io.File;
+
+public class FileTestUtil {
+
+    /**
+     * Returns a {@link File} pointing to a temporary directory. On unix like systems this might be {@code /tmp}
+     * for example.
+     * If {@code suffix} is not null, the returned file points to {@code <temp>/suffix}.
+     *
+     * @param suffix optional path suffix
+     * @return temp directory
+     */
+    public static File getTempDir(String suffix) {
+        String temp = System.getProperty("java.io.tmpdir");
+        if (temp == null) {
+            temp = "tmp";
+        }
+
+        if (suffix == null) {
+            return new File(temp);
+        } else {
+            return new File(temp, suffix);
+        }
+    }
+}
