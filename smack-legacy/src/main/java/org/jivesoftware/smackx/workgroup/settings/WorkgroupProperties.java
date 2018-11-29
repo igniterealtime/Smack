fa7297019daeb0f50183c0db2c9761a26602ebfb
Florian Schmaus <flo@geekplace.eu>
2018-11-29 22:38:11 +0100

    Add checkstyle rule for "Boolean.valueOf()" usages

--- smack-legacy/src/main/java/org/jivesoftware/smackx/workgroup/settings/WorkgroupProperties.java
+++ smack-legacy/src/main/java/org/jivesoftware/smackx/workgroup/settings/WorkgroupProperties.java
@@ -104,7 +104,9 @@ public class WorkgroupProperties extends IQ {
             while (!done) {
                 int eventType = parser.next();
                 if ((eventType == XmlPullParser.START_TAG) && ("authRequired".equals(parser.getName()))) {
+                    // CHECKSTYLE:OFF
                     props.setAuthRequired(Boolean.valueOf(parser.nextText()).booleanValue());
+                    // CHECKSTYLE:ON
                 }
                 else if ((eventType == XmlPullParser.START_TAG) && ("email".equals(parser.getName()))) {
                     props.setEmail(parser.nextText());
