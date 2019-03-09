16dbec2f314eaca6fe2ea605fc1d6fc6cad8c1db
adiaholic <adityaborikar2@gmail.com>
2019-03-09 23:32:32 +0530

    It was an erroneous commit This reverts commit c4485323f9d0d3e3e49367afabb1d78372607be8.

--- smack-extensions/src/main/java/org/jivesoftware/smackx/muc/MultiUserChatManager.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/muc/MultiUserChatManager.java
@@ -19,7 +19,6 @@ package org.jivesoftware.smackx.muc;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -46,6 +45,7 @@ import org.jivesoftware.smack.filter.StanzaTypeFilter;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Stanza;
 import org.jivesoftware.smack.util.Async;
+import org.jivesoftware.smack.util.CleaningWeakReferenceMap;
 
 import org.jivesoftware.smackx.disco.AbstractNodeInformationProvider;
 import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
@@ -144,7 +144,7 @@ public final class MultiUserChatManager extends Manager {
      * those instances to get garbage collected. Note that MultiUserChat instances can not get garbage collected while
      * the user is joined, because then the MUC will have PacketListeners added to the XMPPConnection.
      */
-    private final Map<EntityBareJid, WeakReference<MultiUserChat>> multiUserChats = new HashMap<>();
+    private final Map<EntityBareJid, WeakReference<MultiUserChat>> multiUserChats = new CleaningWeakReferenceMap<>();
 
     private boolean autoJoinOnReconnect;
 
