3c306eaff995c37cc2ea5bb0d673601902b7518f
Florian Schmaus <flo@geekplace.eu>
2019-06-05 10:54:07 +0200

    Improve comment in SmackReactor

--- smack-core/src/main/java/org/jivesoftware/smack/SmackReactor.java
+++ smack-core/src/main/java/org/jivesoftware/smack/SmackReactor.java
@@ -208,7 +208,8 @@ public class SmackReactor {
 
             long selectWait;
             if (nextScheduledAction == null) {
-                // There is no next scheduled action, wait indefinitely in select().
+                // There is no next scheduled action, wait indefinitely in select() or until another thread invokes
+                // selector.wakeup().
                 selectWait = 0;
             } else {
                 selectWait = nextScheduledAction.getTimeToDueMillis();
