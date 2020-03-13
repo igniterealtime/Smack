4beaae7d6ac9a17ca1e2ee91aecfc3a2fbeede04
Florian Schmaus <flo@geekplace.eu>
2020-03-13 16:41:27 +0100

    disco: avoid boxing to Integer when not necessary

--- smack-extensions/src/main/java/org/jivesoftware/smackx/disco/DiscoInfoLookupShortcutMechanism.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/disco/DiscoInfoLookupShortcutMechanism.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2018 Florian Schmaus.
+ * Copyright 2018-2020 Florian Schmaus.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +47,7 @@ public abstract class DiscoInfoLookupShortcutMechanism implements Comparable<Dis
 
     @Override
     public final int compareTo(DiscoInfoLookupShortcutMechanism other) {
-        Integer ourPriority = getPriority();
+        int ourPriority = getPriority();
         return Integer.compare(ourPriority, other.getPriority());
     }
 }
