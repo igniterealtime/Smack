igniterealtime/Smack

commit aa441d743cb0fd2b9fdbdb5848479c5cc37d6223

[bosh] Use ConnectionConfiguration.getHostString() in BOSHConfiguration

smack-bosh/src/main/java/org/jivesoftware/smack/bosh/BOSHConfiguration.java
@@ -77,9 +77,8 @@ public final class BOSHConfiguration extends ConnectionConfiguration {
     }
 
     public URI getURI() throws URISyntaxException {
-        return new URI((https ? "https://" : "http://")
-            + (this.host != null ? this.host : this.hostAddress)
-                + ":" + this.port + file);
+        String uri = https ? "https://" : "http://" + getHostString() + ":" + this.port + file;
+        return new URI(uri);
     }
 
     public Map<String, String> getHttpHeaders() {

commit 8ebe453363c377a51505c2bbd467559a505eec12

[bosh] Fix BOSHConfiguration.getURI()
Reported-by: Damian Minkov <damencho@jitsi.org>
Fixes: aa441d743cb0 ("[bosh] Use ConnectionConfiguration.getHostString() in BOSHConfiguration")

smack-bosh/src/main/java/org/jivesoftware/smack/bosh/BOSHConfiguration.java
@@ -77,7 +77,7 @@ public final class BOSHConfiguration extends ConnectionConfiguration {
     }
 
     public URI getURI() throws URISyntaxException {
-        String uri = https ? "https://" : "http://" + getHostString() + ":" + this.port + file;
+        String uri = (https ? "https://" : "http://") + getHostString() + ":" + this.port + file;
         return new URI(uri);
     }
 
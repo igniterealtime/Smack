8ebe453363c377a51505c2bbd467559a505eec12
Florian Schmaus <flo@geekplace.eu>
2022-05-26 19:52:20 +0200

    [bosh] Fix BOSHConfiguration.getURI()
    Reported-by: Damian Minkov <damencho@jitsi.org>
    Fixes: aa441d743cb0 ("[bosh] Use ConnectionConfiguration.getHostString() in BOSHConfiguration")

--- smack-bosh/src/main/java/org/jivesoftware/smack/bosh/BOSHConfiguration.java
+++ smack-bosh/src/main/java/org/jivesoftware/smack/bosh/BOSHConfiguration.java
@@ -77,7 +77,7 @@ public final class BOSHConfiguration extends ConnectionConfiguration {
     }
 
     public URI getURI() throws URISyntaxException {
-        String uri = https ? "https://" : "http://" + getHostString() + ":" + this.port + file;
+        String uri = (https ? "https://" : "http://") + getHostString() + ":" + this.port + file;
         return new URI(uri);
     }
 
