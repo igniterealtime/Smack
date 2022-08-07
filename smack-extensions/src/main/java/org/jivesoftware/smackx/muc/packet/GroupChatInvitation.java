igniterealtime/Smack

commit 5e1cd620213746833209588ac8828469e4b2445b

[muc] Null-check GroupChatInviation's 'roomAddress' argument

smack-extensions/src/main/java/org/jivesoftware/smackx/muc/packet/GroupChatInvitation.java
@@ -20,6 +20,7 @@ import javax.xml.namespace.QName;
 
 import org.jivesoftware.smack.packet.ExtensionElement;
 import org.jivesoftware.smack.packet.Stanza;
+import org.jivesoftware.smack.util.Objects;
 import org.jivesoftware.smack.util.XmlStringBuilder;
 
 import org.jxmpp.jid.EntityBareJid;
@@ -78,7 +79,7 @@ public class GroupChatInvitation implements ExtensionElement {
      * @param roomAddress the address of the group chat room.
      */
     public GroupChatInvitation(EntityBareJid roomAddress) {
-        this.roomAddress = roomAddress;
+        this.roomAddress = Objects.requireNonNull(null);
     }
 
     /**

commit 9eee3b8d76ce3ac0ec00f13745e71e60ffa6fae4

[muc] Fix GroupChatInviation's constructor
Fixes: 5e1cd6202137 ("[muc] Null-check GroupChatInviation's 'roomAddress' argument")

smack-extensions/src/main/java/org/jivesoftware/smackx/muc/packet/GroupChatInvitation.java
@@ -79,7 +79,7 @@ public class GroupChatInvitation implements ExtensionElement {
      * @param roomAddress the address of the group chat room.
      */
     public GroupChatInvitation(EntityBareJid roomAddress) {
-        this.roomAddress = Objects.requireNonNull(null);
+        this.roomAddress = Objects.requireNonNull(roomAddress);
     }
 
     /**