e389960450b069728a0c038711e089a375b59187
Martin Fidczuk <mfidczuk@microsoft.com>
2022-08-17 15:02:07 +0100

    Inform ourselves of presence info.
    Treat resources belonging to the logged in user as being in the roster, so clients are informed about presence changes on the same account.

--- smack-im/src/main/java/org/jivesoftware/smack/roster/Roster.java
+++ smack-im/src/main/java/org/jivesoftware/smack/roster/Roster.java
@@ -947,7 +947,8 @@ public final class Roster extends Manager {
     }
 
     /**
-     * Returns true if the specified XMPP address is an entry in the roster.
+     * Returns true if the specified XMPP address is an entry in the roster, or is
+     * a resource of the logged in user.
      *
      * @param jid the XMPP address of the user (eg "jsmith@example.com"). The
      *             address must be a bare JID e.g. "domain/resource" or
@@ -955,7 +956,9 @@ public final class Roster extends Manager {
      * @return true if the XMPP address is an entry in the roster.
      */
     public boolean contains(BareJid jid) {
-        return getEntry(jid) != null;
+        RosterEntry entry = getEntry(jid);
+        EntityFullJid user = connection().getUser();
+        return entry != null || (user != null && jid.isParentOf(user));
     }
 
     /**
