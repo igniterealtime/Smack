9e5564a597a98822ca268cef8eb885ce214b11c7
Florian Schmaus <flo@geekplace.eu>
2023-12-07 11:38:25 +0100

    [omemo] Do not swallow the exception in OmemoAesCipher

--- smack-omemo/src/main/java/org/jivesoftware/smackx/omemo/internal/OmemoAesCipher.java
+++ smack-omemo/src/main/java/org/jivesoftware/smackx/omemo/internal/OmemoAesCipher.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2017 Paul Schaub, 2019-2021 Florian Schmaus
+ * Copyright 2017 Paul Schaub, 2019-2023 Florian Schmaus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +47,7 @@ public class OmemoAesCipher {
             String message = "Unable to perform " + OmemoConstants.Crypto.CIPHERMODE
                             + " operation requires by OMEMO. Ensure that a suitable crypto provider for is available."
                             + " For example Bouncycastle on Android (BouncyCastleProvider)";
-            throw new AssertionError(message);
+            throw new AssertionError(message, e);
         }
     }
 
