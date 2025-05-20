1f299d48ea21949efa88c40d3b2b358c5e473e95
Boris Grozev <boris@jitsi.org>
2025-05-20 13:59:14 -0500

    test: Add a performance test for XmlStringBuilder.toString().

--- smack-core/src/test/java/org/jivesoftware/smack/util/XmlStringBuilderTest.java
+++ smack-core/src/test/java/org/jivesoftware/smack/util/XmlStringBuilderTest.java
@@ -22,6 +22,8 @@ import org.jivesoftware.smack.test.util.XmlAssertUtil;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.*;
+
 public class XmlStringBuilderTest {
 
     /**
@@ -43,5 +45,94 @@ public class XmlStringBuilderTest {
         StringBuilder actualXmlTwo = actualXml.toXML(XmlEnvironment.EMPTY);
 
         XmlAssertUtil.assertXmlSimilar(expectedXml, actualXmlTwo);
+    }
+
+    /**
+     * XmlStringBuilder toString may be somewhat slow with many nested elements.
+     *
+     * This is meant to be run manually.
+     */
+    // @Test
+    public void performanceTest() {
+        final int countOuter = 500;
+        final int countInner = 50;
+
+        System.err.println("XmlStringBuilder.toString() performance");
+        test1(countOuter, countInner);
+        test2(countOuter, countInner);
+        test3(countOuter, countInner);
+    }
+
+    private static void test1(int countOuter, int countInner) {
+        System.err.println("Test 1");
+        XmlStringBuilder parent = new XmlStringBuilder();
+        XmlStringBuilder child = new XmlStringBuilder();
+        XmlStringBuilder child2 = new XmlStringBuilder();
+
+        for (int i = 1; i < countOuter; i++) {
+            XmlStringBuilder cs = new XmlStringBuilder();
+            for (int j = 0; j < countInner; j++) {
+                cs.append("abc");
+            }
+            child2.append((CharSequence) cs);
+        }
+
+        child.append((CharSequence) child2);
+        parent.append((CharSequence) child);
+
+        time("test1: parent", () -> "len=" + parent.toString().length());
+        time("test1: child", () -> "len=" + child.toString().length());
+        time("test1: child2", () -> "len=" + child2.toString().length());
+    }
+
+    private static void test2(int countOuter, int countInner) {
+        System.err.println("Test 2: evaluate children first");
+        XmlStringBuilder parent = new XmlStringBuilder();
+        XmlStringBuilder child = new XmlStringBuilder();
+        XmlStringBuilder child2 = new XmlStringBuilder();
+
+        for (int i = 1; i < countOuter; i++) {
+            XmlStringBuilder cs = new XmlStringBuilder();
+            for (int j = 0; j < countInner; j++) {
+                cs.append("abc");
+            }
+            child2.append((CharSequence) cs);
+        }
+
+        child.append((CharSequence) child2);
+        parent.append((CharSequence) child);
+
+        time("test2: child2", () -> "len=" + child2.toString().length());
+        time("test2: child", () -> "len=" + child.toString().length());
+        time("test2: parent", () -> "len=" + parent.toString().length());
+    }
+
+    private static void test3(int countOuter, int countInner) {
+        System.err.println("Test 3: use append(XmlStringBuilder)");
+        XmlStringBuilder parent = new XmlStringBuilder();
+        XmlStringBuilder child = new XmlStringBuilder();
+        XmlStringBuilder child2 = new XmlStringBuilder();
+
+        for (int i = 1; i < countOuter; i++) {
+            XmlStringBuilder cs = new XmlStringBuilder();
+            for (int j = 0; j < countInner; j++) {
+                cs.append("abc");
+            }
+            child2.append(cs);
+        }
+
+        child.append(child2);
+        parent.append(child);
+
+        time("test3: parent", () -> "len=" + parent.toString().length());
+        time("test3: child", () -> "len=" + child.toString().length());
+        time("test3: child2", () -> "len=" + child2.toString().length());
+    }
+
+    private static void time(String name, Supplier<String> block) {
+        long start = System.currentTimeMillis();
+        String result = block.get();
+        long end = System.currentTimeMillis();
+        System.err.println(name + " took " + (end - start) + "ms: " + result);
     }
 }
