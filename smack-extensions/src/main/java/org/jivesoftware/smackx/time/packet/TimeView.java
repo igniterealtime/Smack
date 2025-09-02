7ebe0e7a601d3539f3a95d70cc6d3cd5382d6402
Florian Schmaus <flo@geekplace.eu>
2025-09-02 17:18:53 +0200

    [time] Use ZonedDateTime in o.j.smackx.time

--- smack-extensions/src/main/java/org/jivesoftware/smackx/time/packet/TimeView.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/time/packet/TimeView.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2021 Florian Schmaus
+ * Copyright 2021-2025 Florian Schmaus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,22 +16,41 @@
  */
 package org.jivesoftware.smackx.time.packet;
 
+import java.time.ZonedDateTime;
+
 import org.jivesoftware.smack.packet.IqView;
 
 public interface TimeView extends IqView {
+
+    /**
+     * Returns the time or <code>null</code> if the time hasn't been set.
+     *
+     * @return the time.
+     */
+    ZonedDateTime getZonedDateTime();
 
     /**
      * Returns the time as a UTC formatted String using the format CCYY-MM-DDThh:mm:ssZ.
      *
      * @return the time as a UTC formatted String.
      */
-    String getUtc();
+    default String getUtc() {
+        var zonedDateTime = getZonedDateTime();
+        if (zonedDateTime == null) return null;
+
+        return getZonedDateTime().toInstant().toString();
+    };
 
     /**
      * Returns the time zone.
      *
      * @return the time zone.
      */
-    String getTzo();
+    default String getTzo() {
+        var zonedDateTime = getZonedDateTime();
+        if (zonedDateTime == null) return null;
+
+        return zonedDateTime.getZone().normalized().getId();
+    };
 
 }
