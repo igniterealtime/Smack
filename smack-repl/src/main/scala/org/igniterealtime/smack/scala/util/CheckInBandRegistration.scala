a6e395d9448c28ace0989e1f89572cb36195705a
Florian Schmaus <flo@geekplace.eu>
2026-08-12 17:35:32 +0200

    [repl] Bump Scala to 3.3.7
    And also use scalafix and scalafmt instead of scalastyle

--- smack-repl/src/main/scala/org/igniterealtime/smack/scala/util/CheckInBandRegistration.scala
+++ smack-repl/src/main/scala/org/igniterealtime/smack/scala/util/CheckInBandRegistration.scala
@@ -1,51 +1,40 @@
-/**
- *
- * Copyright 2018 Florian Schmaus
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/** Copyright 2018-2026 Florian Schmaus
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+  * compliance with the License. You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software distributed under the License is
+  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and limitations under the License.
+  */
 package org.igniterealtime.smack.scala.util
 
 import scala.language.postfixOps
 
-import org.jxmpp.jid.DomainBareJid
-import org.jxmpp.jid.impl.JidCreate
-
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
-
 import org.jivesoftware.smackx.iqregister.AccountManager
+import org.jxmpp.jid.impl.JidCreate
+import org.jxmpp.jid.DomainBareJid
 
-object CheckInBandRegistration {
+object CheckInBandRegistration:
 
-  def supportsIbr(xmppServiceString: String) : Boolean = {
-    val xmppServiceAddress = JidCreate domainBareFrom(xmppServiceString)
+  def supportsIbr(xmppServiceString: String): Boolean =
+    val xmppServiceAddress = JidCreate domainBareFrom (xmppServiceString)
 
-    val config = XMPPTCPConnectionConfiguration.builder()
-                  .setXmppDomain(xmppServiceAddress)
-                  .build();
+    val config = XMPPTCPConnectionConfiguration
+      .builder()
+      .setXmppDomain(xmppServiceAddress)
+      .build();
 
     supportsIbr(config)
-  }
 
-  def supportsIbr(connectionConfiguration: XMPPTCPConnectionConfiguration) : Boolean = {
+  def supportsIbr(connectionConfiguration: XMPPTCPConnectionConfiguration): Boolean =
     val connection = new XMPPTCPConnection(connectionConfiguration)
     connection.connect
-    try {
+    try
       AccountManager.getInstance(connection).supportsAccountCreation
-    } finally {
+    finally
       connection disconnect
-    }
-  }
-
-}
